ee49652f1e52faa69b84183a0175fb1efe356518
Luka Brinar <luka.brinar@dlabs.si>
2019-06-21 12:55:19 +0200

    Add login and register route to api

--- api/app.js
+++ api/app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/auth/register/register');
+var loginRouter = require('./routes/auth/login/login');
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth/register', registerRouter);
+app.use('/auth/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/login/login-component/login.component.js
+++ app/login/login-component/login.component.js
@@ -12,21 +12,28 @@
         .module('LoginModule')
         .component('loginComponent', loginComponent);
 
-    function loginController(userService, $location) {
+    function loginController(userService, $location, $http) {
         
         var $ctrl = this;
 
         $ctrl.login = login;
+
+        $ctrl.register = register;
         
         //////////////////////////////
         
-        
-
         function login () {
-            var currentUser = userService.checkUser($ctrl.userName, $ctrl.password);
-            if (currentUser) {
-                $location.path('/todo');
-            }
+            $http.get("http://localhost:3000/auth/login")
+                .then(function(response) {
+                    console.log(response.data);
+            });
+        }
+
+        function register () {
+            $http.get("http://localhost:3000/auth/register")
+                .then(function(response) {
+                    console.log(response.data);
+            });
         }
 
     }
